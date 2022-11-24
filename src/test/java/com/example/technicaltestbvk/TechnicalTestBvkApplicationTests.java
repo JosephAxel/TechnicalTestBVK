6642d2e41d6a764a41e69a92e9bbf12481846bcf
JosephAxel <josep021212@gmail.com>
2022-11-24 15:23:45 +0700

    test

---
+++ src/test/java/com/example/technicaltestbvk/TechnicalTestBvkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technicaltestbvk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestBvkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
