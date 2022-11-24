a86d30654d12fb215657222c9f0886cf2a91a7a1
JosephAxel <josep021212@gmail.com>
2022-11-24 16:09:40 +0700

    init project

---
+++ src/main/java/com/example/TechnicalTestBVK/TechnicalTestBvkApplication.java
@@ -0,0 +1,13 @@
+package com.example.TechnicalTestBVK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestBvkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestBvkApplication.class, args);
+	}
+
+}
